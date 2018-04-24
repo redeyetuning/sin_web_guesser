582ea1ede50ad0c917ecb9eb82737da106393109
Samuel Masters <redeyetuning@gmail.com>
2018-04-24 14:25:36 +0400

    I5: Add 5 guess maximum limit before reset.

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,39 +1,51 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(101)
-
-message = ""
+@@secret_number = rand(101)
+@@message = ""
 number = ""
 bg_colour = "none"
+@@guesses = 5
 
-def set_number(message)
-	"The SECRET NUMBER is #{SECRET_NUMBER}" if message == "You got it right"  
+def set_number
+	"The secretT NUMBER is #{@@secret_number}" if @@message == "You got it right"  
 end
 
-def set_bg_colour(message)
+def set_bg_colour
 	case 
-	when (message == "Way too high!" or message == "Way too low!") then "red"
-	when (message == "Too high!" or message == "Too low!") then "LightPink"
-	when message == "You got it right!" then "green"
+	when (@@message == "Way too high!" or @@message == "Way too low!") then "red"
+	when (@@message == "Too high!" or @@message == "Too low!") then "LightPink"
+	when @@message == "You got it right!</br></br> A new number has been generated" then "green"
 	else "none"
 	end
 end
 
 def check_guess(guess)
+	@@guesses -= 1
+	if guess == ""
+	elsif guess.to_i > @@secret_number
+		guess.to_i - @@secret_number > 5 ? "Way too high!" : "Too high!"
+	elsif guess.to_i < @@secret_number
+		@@secret_number - guess.to_i > 5 ? "Way too low!" : "Too low!"
+	elsif guess.to_i == @@secret_number
+		@@guesses = "x"
+	end
+end
+
+def guess_0
 	
-	if guess.to_i > SECRET_NUMBER
-		guess.to_i - SECRET_NUMBER > 5 ? "Way too high!" : "Too high!"
-	elsif guess.to_i < SECRET_NUMBER
-		SECRET_NUMBER - guess.to_i > 5 ? "Way too low!" : "Too low!"
-	elsif guess.to_i == SECRET_NUMBER
-		"You got it right!"
+	if @@guesses == 0 or @@guesses == "x"
+		@@secret_number = rand(101)
+		@@guesses == 0 ? @@message = "You ran out of guesses</br></br>A new number has been generated" : @@message = "You got it right!</br></br> A new number has been generated" 	
+	  @@guesses = 5
+	else ""
 	end
 end
 
 get '/' do
-		message = check_guess(params["guess"])
-		number = set_number(message)
-		bg_colour = set_bg_colour(message)
-		erb :index, :locals => {:message => message, :number => number, :bg_colour => bg_colour}
+	@@message = check_guess(params["guess"])
+	number = set_number
+	guess_0
+	bg_colour = set_bg_colour
+	erb :index, :locals => {:message => @@message, :number => number, :bg_colour => bg_colour}
 end
